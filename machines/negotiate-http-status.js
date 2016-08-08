7679af335b1de7ec9c41d499053660e284c81ef3
Scott Gress <scottmgress@gmail.com>
2016-08-08 10:25:01 -0500

    Added "negotiate-http-status" machine + tests

---
+++ machines/negotiate-http-status.js
@@ -0,0 +1,102 @@
+module.exports = {
+
+
+  friendlyName: 'Negotiate HTTP status code',
+
+
+  description: 'Trigger a specific exit based on the status code returned with an HTTP response.',
+
+
+  sync: true,
+
+
+  inputs: {
+
+    statusCode: {
+      description: 'The status code to negotiate',
+      example: 403
+    }
+
+  },
+
+
+  exits: {
+
+    success: {
+      description: 'A 2xx status code was returned.',
+    },
+
+    redirect: {
+      description: 'A 3xx status code was returned,',
+    },
+
+    badRequest: {
+      description: '400 status code was returned.',
+    },
+
+    unauthorized: {
+      description: '401 status code was returned.',
+    },
+
+    forbidden: {
+      description: '403 status code was returned.',
+    },
+
+    notFound: {
+      description: '404 status code was returned.',
+    },
+
+    otherClientError: {
+      description: 'A 4xx status code other than 400, 401, 403 or 404 was returned.'
+    },
+
+    serverError: {
+      description: '5xx status code was returned.',
+    },
+
+    other: {
+      description: 'An unusual / unsupported status code was returned.'
+    }
+
+  },
+
+
+  fn: function(inputs, exits) {
+
+    if (inputs.statusCode < 200 || inputs.statusCode > 599) {
+      return exits.other();
+    }
+
+    if (inputs.statusCode >= 200 && inputs.statusCode < 300) {
+      return exits.success();
+    }
+
+    if (inputs.statusCode >= 300 && inputs.statusCode < 400) {
+      return exits.redirect();
+    }
+
+    if (inputs.statusCode >= 400 && inputs.statusCode < 500) {
+      if (inputs.statusCode === 400) {
+        return exits.badRequest();
+      }
+      if (inputs.statusCode === 401) {
+        return exits.unauthorized();
+      }
+      if (inputs.statusCode === 403) {
+        return exits.forbidden();
+      }
+      if (inputs.statusCode === 404) {
+        return exits.notFound();
+      }
+      return exits.otherClientError();
+    }
+
+    if (inputs.statusCode >= 500 && inputs.statusCode < 600) {
+      return exits.serverError();
+    }
+
+    return exits.other();
+
+  },
+
+};
